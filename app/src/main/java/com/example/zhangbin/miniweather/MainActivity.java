29fea804482af3441b71f5914b569ef1d39d1559
zhangbin <zhangbin@zhangbindeMacBook-Air.local>
2016-09-21 09:54:30 +0800

    intit project

---
+++ app/src/main/java/com/example/zhangbin/miniweather/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.zhangbin.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
